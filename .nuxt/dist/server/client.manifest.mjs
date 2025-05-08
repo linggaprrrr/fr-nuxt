04add643da0bffa9467988832378e3947521dc5c
lingga pangestu rachman <pangestur11@gmail.com>
2025-05-08 10:43:58 +0700

    Delete .nuxt directory

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Applications/MAMP/htdocs/nuxtjs/dufansnap/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "blank" | "components-default-layout-with-vertical-nav" | "components-footer" | "components-nav-items" | "components-navbar-theme-switcher" | "components-user-layout-with-vertical-nav" | "components-user-nav-items" | "components-user-profile" | "default" | "user"
-declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
